27632ba093be14977613976e5a740472af88d2dc
Vladislav <jukov801@gmail.com>
2024-04-14 15:39:44 +0300

    A function for inverting bits has been created.

--- Bitwise-Operations/main.cpp
+++ Bitwise-Operations/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <bitset>
+#include <chrono>
 
-int main(int argc, char* argv[]) {
-  printf("Hello World!");
+uint8_t Inversion(uint8_t *begin, uint8_t *end, const uint8_t &data, const uint8_t &mask) {
+  if (begin == end) {
+    return 0;
+  }
+  
+  uint8_t result = data;
+  for (; begin != end; ++begin) {
+    result ^= mask;
+  }
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  uint8_t data = 0b10101010;
+  uint8_t mask = (1u << 2);
+  
+  auto start = std::chrono::high_resolution_clock::now();
+  uint8_t result = Inversion(&data, &data + 1, data, mask);
+  auto end = std::chrono::high_resolution_clock::now();
+  
+  std::cout << "Result: " << std::bitset<8>(result) << std::endl;
+  std::cout << "Inversion: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "mcs \n";
+  
   return 0;
 }
