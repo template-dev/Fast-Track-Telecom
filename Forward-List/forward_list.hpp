7789a35f23433c2e83188f7407391e283e5372a9
Vladislav <jukov801@gmail.com>
2024-04-20 15:41:05 +0300

    Created a basic project for forward list

---
+++ Forward-List/forward_list.hpp
@@ -0,0 +1,40 @@
+#ifndef FAST_TRACK_TELECOM_FORWARD_LIST_HPP
+#define FAST_TRACK_TELECOM_FORWARD_LIST_HPP
+
+template<typename T>
+class forward_list {
+public:
+  forward_list() noexcept;
+  explicit forward_list(const T& value) noexcept;
+  
+  forward_list(const forward_list& other) noexcept;
+  forward_list(forward_list&& other) noexcept;
+  forward_list& operator=(const forward_list& other) noexcept;
+  forward_list& operator=(const forward_list&& other) noexcept;
+  
+  void push_back(const T& value);
+  void push_front(const T& value);
+  void pop_back();
+  void pop_front();
+  void insert(size_t pos, const T& value);
+  void erase(const T& value);
+  void clear();
+  bool isEmpty() const;
+  
+  forward_list();
+  
+private:
+  template<typename N>
+  struct Node {
+  Node(N data = N{}, Node* next = nullptr)
+    : data_{data}
+    , next_{next}
+    {}
+    N data_;
+    Node* next_;
+  };
+  Node<T>* pHead_;
+  size_t count_;
+};
+
+#endif // FAST_TRACK_TELECOM_FORWARD_LIST_HPP
---
+++ Forward-List/main.cpp
@@ -0,0 +1,7 @@
+#include "forward_list.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  
+  return 0;
+}
