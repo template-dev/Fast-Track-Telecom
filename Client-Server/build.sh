7a6f43a943cc8dcaf9c2eb8a77a4ac0e68868b51
Vladislav <jukov801@gmail.com>
2024-04-26 21:39:18 +0300

    .

--- Client-Server/build.sh
+++ Client-Server/build.sh
@@ -3,7 +3,7 @@ rm -r build/*
 mkdir src
 mkdir build
 asn1c Registration.asn1 -D src -no-gen-OER -no-gen-example
-gcc -Isrc -o build/coder Registration_coder.c src/*.c -DPDU=Rectangle -DASN_DISABLE_OER_SUPPORT
-gcc -Isrc -o build/decoder Registration_decoder.c src/*.c -DPDU=Rectangle -DASN_DISABLE_OER_SUPPORT
+gcc -Isrc -o build/coder client_coder.cpp src/*.c -DPDU=Rectangle -DASN_DISABLE_OER_SUPPORT
+gcc -Isrc -o build/decoder client_decoder.cpp src/*.c -DPDU=Rectangle -DASN_DISABLE_OER_SUPPORT
 ./build/coder ./build/Registration_ber_out
 ./build/decoder ./build/Registration_ber_out
